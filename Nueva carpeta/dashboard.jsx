9d12c6f3e3f71142248aab87bb83f3b1d7543066
alex-Doick <116039578+alex-Doick@users.noreply.github.com>
2023-12-05 23:51:31 -0500

    Add files via upload

---
+++ Nueva carpeta/dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class dashboard extends React.Component{
+
+
+    render(){
+        return(
+            <div>
+                Hola desde dashboard
+            </div>
+        );
+    }
+}
+export default dashboard
