e53ffba309338a77289a7687f1e4a4a5dbbde0cc
luisMorales13123 <93411473+luisMorales13123@users.noreply.github.com>
2024-05-14 16:21:13 -0500

    Add files via upload

---
+++ docs/TRISMESTRE VI/Evidencias/Luis Fernando Morales Camacho/Trabajo dart/1.1.dart
@@ -0,0 +1,14 @@
+void main() {
+  //creamos la variables 
+  var nombre = "luis";
+  var sexo = 'm';
+  var edad = 19;
+  var salario = 158323.15;
+  var transporte = true ;
+ 
+
+
+//imprimimos las variables atravez de una contatenacion $VARIABLE
+  print('tus nombre es $nombre tienes $edad años y  tu genero es $sexo');
+  print('tu salario es $salario  y tu disponibilidad de vehiculo es $transporte');
+}
