e12781d443c4cf90e92fe250fffeab15ea889781
luismorales <luis123445666@gmail.com>
2024-06-26 14:08:01 -0500

    Cambio de aplicativo flutter

---
+++ docs/TRISMESTRE VI/Aplicativo movil/api/api/lib/main.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login y Listar desde Render',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login(BuildContext context) async {
+    // Aquí deberías agregar la lógica de autenticación
+    // Por ejemplo, podrías realizar una solicitud a tu API de autenticación
+    // Para este ejemplo, simplemente navegaremos a la pantalla de datos
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _login(context),
+              child: Text('Entrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, dynamic>> _tipoDocumentos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://novaa-tech-6.onrender.com/api/TipoDeDocumento/Documento/'));
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = json.decode(response.body);
+        setState(() {
+          _tipoDocumentos = jsonResponse
+              .map((item) => {
+                    'id': item['id'],
+                    'TipoDocumento': item['TipoDocumento'],
+                  })
+              .toList();
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tipos de Documento'),
+      ),
+      body: _tipoDocumentos.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: DataTable(
+                  columnSpacing: MediaQuery.of(context).size.width * 0.1,  // Adjust column spacing
+                  columns: [
+                    DataColumn(
+                      label: Text(
+                        'ID',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Tipo de Documento',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                  rows: _tipoDocumentos
+                      .map(
+                        (doc) => DataRow(cells: [
+                          DataCell(Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(color: Colors.black12),
+                              ),
+                            ),
+                            child: Text(doc['id'].toString()),
+                          )),
+                          DataCell(Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(color: Colors.black12),
+                              ),
+                            ),
+                            child: Text(doc['TipoDocumento']),
+                          )),
+                        ]),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+    );
+  }
+}
